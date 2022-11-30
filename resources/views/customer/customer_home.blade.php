16e3e79fa045d1d046a78aa0f20af58104523704
AntonioCR11 <achristopher989@gmail.com>
2022-11-30 09:09:34 +0700

    revisi progress 1 - 2

--- resources/views/customer/customer_home.blade.php
+++ resources/views/customer/customer_home.blade.php
@@ -23,7 +23,13 @@
 @endsection
 
 @section('custom_css')
-    <link rel="stylesheet" href="{{asset('build/css/admin_customer.css')}}">
+    <link rel="stylesheet" href="{{asset('build/css/customer_home.css')}}">
+    <style>
+        .navigation:hover{
+            transform: scale(1.2);
+            font-weight: 600
+        }
+    </style>
 @endsection
 
 @section('dependencies')
@@ -31,11 +37,75 @@
 @endsection
 
 @section('content')
-    <div class="row m-0" style="height: 100vh">
+    <div class="container">
+
+        {{-- NAVBAR --}}
+        <ul class="nav py-3">
+            <div class="row w-100">
+                {{-- LOGO --}}
+                <div class="col">
+                    <h1 class="m-0" style="cursor: pointer;">
+                        <span class="text-light bg-dark p-3">
+                            ρσятαвℓє
+                        </span>
+                    </h1>
+                </div>
+                {{-- NAVIGATION --}}
+                <div class="col d-flex justify-content-center align-items-center">
+                    <li class="nav-item" style="border-bottom: 1px solid black">
+                        <a class="nav-link text-dark navigation" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark navigation" href="#">Search</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark navigation" href="#">Favorite</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark navigation" href="#">History</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark navigation" href="#">Profile</a>
+                    </li>
+                </div>
+                {{-- PROFILE --}}
+                <div class="col d-flex justify-content-end align-items-center">
+                    <div class="notification me-4" style="cursor: pointer">
+                        <img class="navigation" src="{{asset("images/admin/notification.png")}}" alt="" width="30px">
+                    </div>
+                    <div class="profile">
+                        <img src="{{asset("images/customer/pp.jpg")}}" alt="" width="45px" height="45px" style="border-radius: 50%">
+                    </div>
+                </div>
+            </div>
+        </ul>
+
+        {{-- JUMBOTRON --}}
+        <div class="jumbotron row m-0 w-100" style="height: 60vh;">
+            <div class="col-6 d-flex justify-content-end align-items-center" >
+                <div class="left_content">
+
+                    {{-- BANNER TEXT --}}
+                    <div class="banner_text">
+                        <p class="text-end " style="font-size: 3.5em;font-family: helvetica_bold">A New Way</p>
+                        <p class="text-end " style="font-size: 3.5em;font-family: helvetica_bold;margin-top:-20px;">To Order a Table</p>
+                        <p class="text-end " style="font-family: helvetica_regular;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dignissimos nobis ad sequi aliquid impedit iusto officia ea enim? Cum eius dolore labore tempore assumenda, quia quas cupiditate? Officia, voluptatum.</p>
+                    </div>
+
+                    {{-- BUTTON --}}
+                    <div class="d-flex justify-content-end mt-4">
+                        <div class="btn me-2" style="background-color: #ed3b27;color:white;">Explore Restaurant</div>
+                        <div class="btn" style="border:1px solid #ed3b27; color: #ed3b27;">Favorite</div>
+                    </div>
 
 
+                </div>
+            </div>
+            <div class="col-6 d-flex justify-content-end align-items-center">
+                <img src="{{asset('images/customer/banner1.png')}}" width="100%" alt="">
+            </div>
+        </div>
     </div>
-
 @endsection
 
 @section('js_script')
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,6 +24,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <link rel="icon" type="image/x-icon" href="{{asset('images/logo/logo.png')}}">
+    <style>
+        html,body{
+            background-color: rgb(240, 240, 240);
+        }
+        p,h1,h2,h3,h4,h5,h6{
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+    </style>
 </head>
 <body>
     @yield('header')
